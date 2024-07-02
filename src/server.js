f0c92e2d5cc9ca66e6712f839ff536c3c1e9bf1b
alan <alanwengrzee@gmail.com>
2024-07-02 13:47:48 -0300

    add .env.example and add pm2 to app run full time

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config")
 
 const uploadConfig = require('./configs/upload');
 
@@ -36,7 +37,7 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
